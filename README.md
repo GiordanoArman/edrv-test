030e084c90ab1012de7716cba6f1882014df1ef6
Giordano Arman <giordanoarman@gmail.com>
2020-03-15 14:39:10 +0100

    Improved comments.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This app:
 
- 1. Uses the most efficient way of querying the API which is going as near as possible to the 5 minutes minimum interval allowed by the API provider.
+ 1. Uses the most efficient way of querying the API which is keeping as near as possible to the 5 minutes minimum interval allowed by the API provider.
  2. Can respect the API constraint if it crashes and is restarted, since it takes note of its operations on a persistent database.
  3. Can operate if the database is corrupt.
  4. Takes into account CPU clock errors and avoids banning due to them.
@@ -19,6 +19,10 @@ This app:
 
  1. After installing `cd` into the project folder.
  2. Run `npm run start`.
+ 
+## Codebase overview
+
+All the source files are located in the `src` folder: `index.js` contains all the important logic, `start.js` imports `index.js` and boots the application.
 
 ## Why the app waits for the server response to start waiting for the next call
 
@@ -38,11 +42,11 @@ Let's examine the polling flow:
  4. Their app sends response to network.
  5. Our app receives the response.
 
-Between point A and C there can be variable time amounts taken by our requests, depending on the network performance, traffic going through their infrastructure and how much their machines are busy handling other tasks at each of our requests. Sending requests at regular intervals with solutions like `setInterval` can cause problems in situations like the following:
+Between point 1 and 3 there can be variable time amounts taken by our requests, depending on the network performance, traffic going through their infrastructure and how much their machines are busy handling other tasks at each of our requests. Sending requests at regular intervals with solutions like `setInterval` can cause problems in situations like the following:
 
  - Our app sends a message at 00:00:00.
  - The network performs poorly, their infrastructure is saturated and their app checks whether we comply with the constraint at 00:00:10 (ten seconds later) and responds at 00:00:11.
  - Our app sends a second message at 00:05:00 (five minutes after our first message was sent).
  - The networks performs better and their infrastructure is less busy, so their app checks on our constraint compliance at 00:05:01 (1 second later it was sent). Their app will detect that only 4 minutes and 51 seconds have passed since our initial message. At this point we are banned.
  
- Knowing for certain that the amount of time between point 1 and 3 is always less or equal to the time between point 1 and 5, if we wait for point 5 (the app gets the response) to happen before setting the next polling call we are sure to avoid the just mentioned banning situation.
+ Knowing for certain that the amount of time between point 1 and 3 is always less or equal to the time between point 1 and 5, if we wait for point 5 (the app gets the response) to happen before setting the wait time for the next polling call we are sure to avoid the just mentioned banning situation.
--- src/index.js
+++ src/index.js
@@ -4,21 +4,34 @@ const path = require("path");
 
 const DB_LOCATION = path.join(__dirname, "db.json");
 
-// official minimum query interval allowed by the API provider. It is set to 5 minutes in milliseconds.
+// official minimum query interval allowed by the API provider. It is set to 5 
+// minutes in milliseconds.
 const API_MIN_INTERVAL_MS = 1000 * 60 * 5;
 
 /*
-  time margin that will be added to the time between requests. As mentioned here https://www.eecis.udel.edu/~ntp/ntpfaq/NTP-s-sw-clocks.htm hardware clocks are not accurate. They can gain an error of ~1 second over the course of one day. The paper mentions Linux machines can have a 11 PPM error rate (part per million, see the paper for an explaination of this measurement unit). I'll round it to 20 PPM and assume a worst case scenario of one party (eg. the app server) being 20 PPM short and the other (eg. their server) 20 PPM over in the 5 minutes estimate. So we can later add a 40 PPM margin to the API minimum interval so the app is going to avoid being banned due to CPU clock errors.
+  time margin that will be added to the time between requests. As mentioned here 
+  https://www.eecis.udel.edu/~ntp/ntpfaq/NTP-s-sw-clocks.htm hardware clocks are 
+  not accurate. They can gain an error of ~1 second over the course of one day. 
+  The paper mentions Linux machines can have a 11 PPM error rate (part per 
+  million, see the paper for an explaination of this measurement unit). I'll 
+  round it to 20 PPM and assume a worst case scenario of one party (eg. the app 
+  server) being 20 PPM short and the other (eg. their server) 20 PPM over in the 
+  5 minutes estimate. We can later add a 40 PPM margin to the API minimum 
+  interval so the app is going to avoid being banned due to CPU clock errors.
 */
 const CPU_TOLERANCE_MS = (API_MIN_INTERVAL_MS / 1000000) * 40;
 
 // actual wait time for queries in milliseconds, adjusted for CPU clock errors
 const WAIT_TIME_MS = API_MIN_INTERVAL_MS + CPU_TOLERANCE_MS;
 
-// assumed max value of time in milliseconds (1 minute) that can occur between the time the query is sent and the moment it is checked for constraint compliance by the API. This is used rarely by the app, only when it does not know the response date for the last query.
+// assumed max value of time in milliseconds (1 minute) that can occur between 
+// the time the query is sent and the moment it is checked for constraint 
+// compliance by the API. This is used rarely by the app, only when it does not 
+// know the response date for the last query.
 const MAX_QUERY_RECEPTION_DELAY_MS = 1000 * 60;
 
-// default database that gets used on first boot or on boot with corrupt database
+// default database that gets used on first boot or on boot with corrupt 
+// database
 const DEFAULT_DB = { 
   lastAPIResponseTime: null, 
   lastAPIQueryTime: null
@@ -28,7 +41,9 @@ async function main(options) { // function invoked by src/start.js on boot
   const db = getDB(DB_LOCATION);
   
   if (db.corrupt) {
-    // the app may have previously crashed while writing on db, since we don't know anything about the dates of the last operations, we wait the assumed max query reception delay + regular wait time
+    // the app may have previously crashed while writing on db, since we don't 
+    // know anything about the dates of the last operations, we wait the assumed 
+    // max query reception delay + regular wait time
     await time(MAX_QUERY_RECEPTION_DELAY_MS + WAIT_TIME_MS);
   }
   
@@ -47,7 +62,10 @@ async function main(options) { // function invoked by src/start.js on boot
     let res;
     try {
       res = await axios.get(options.url);
-      // the app waits for the response of the server to continue the iteration because a reliable date is needed on which it can start to wait for the computed time interval to finish (more in the README.md, "Why the app waits for the server response to start waiting for the next call")
+      // the app waits for the response of the server to continue the iteration 
+      // because a reliable date is needed on which it can start to wait for the 
+      // computed time interval to finish (more in the README.md, "Why the app 
+      // waits for the server response to start waiting for the next call")
     } catch (error) {
       if (error && error.isAxiosError) {
         lastResponse = Date.now(); // write the response time
@@ -77,37 +95,57 @@ function getStatus(res) {
   return null;
 }
 
-// this function waits the necessary time according to file constants and API communication dates
+// this function waits the necessary time according to file constants and API 
+// communication dates
 async function waitBeforePoll(lastResponse, lastQuery) {
   if (isNumber(lastQuery)) { // app previously polled the API
     if (isNumber(lastResponse)) { // app had at least one API response
       if (lastResponse < lastQuery) {
-        // [*]
-        // app did not get a response for its last query. The app could have been terminated while waiting on the response. At this point the app can't know when the API processed the query (worst case) or if the API got the query at all (best case). If the API did not get the query a safe period to query again starts at lastResponse + WAIT_TIME_MS. If the API received the query we assume it happened before lastQuery + MAX_QUERY_RECEPTION_DELAY_MS (NOTE: it's a risky assumption, because we don't actually know for sure how long it will take for the message to reach the API). Therefore in this case a safe period to query again starts at lastQuery + MAX_QUERY_RECEPTION_DELAY_MS + WAIT_TIME_MS. The safest way to deal with these uncertainties is to satisfy the worst case requirements.
+        /*
+         [*]
+         app did not get a response for its last query. The app could have been 
+         previously terminated while waiting on the response. At this point the 
+         app can't know when the API processed the query (worst case) or if the 
+         API got the query at all (best case). If the API did not get the query 
+         a safe period to query again starts at lastResponse + WAIT_TIME_MS. If 
+         the API received the query we assume it happened before 
+         lastQuery + MAX_QUERY_RECEPTION_DELAY_MS (NOTE: it's a risky 
+         assumption, because we don't actually know for sure how long it will 
+         take for the message to reach the API). Therefore in this case a safe 
+         period to query again starts at 
+         lastQuery + MAX_QUERY_RECEPTION_DELAY_MS + WAIT_TIME_MS. The safest way 
+         to deal with these uncertainties is to satisfy the worst case 
+         requirements.
+        */
         await necessaryTime(
           lastQuery, MAX_QUERY_RECEPTION_DELAY_MS + WAIT_TIME_MS);
       } else {
         await necessaryTime(lastResponse, WAIT_TIME_MS);
       }
     } else {
-      // app never got a response since its installation; see block above with asterisk [*]
+      // app never got a response since its installation; see block above with 
+      // asterisk [*]
       await necessaryTime(
         lastQuery, MAX_QUERY_RECEPTION_DELAY_MS + WAIT_TIME_MS);
     }
   }
 }
 
-// this function waits only for the duration of time that has not already passed since waitStartDate
+// this function waits only for the duration of time that has not already passed 
+// since waitStartDate
 async function necessaryTime(waitStartDate, timeToWait) {
   const timePassed = Date.now() - waitStartDate;
   if (timePassed >= timeToWait) {
-    // app does not wait at all if it already waited enough, eg. the waitStartDate is set to two days ago because the app crashed around that time and it was rebooted only two days later
+    // app does not wait at all if it already waited enough, eg. the 
+    // waitStartDate is set to two days ago because the app crashed around that 
+    // time and it was rebooted only two days later
     return; 
   }
   await time(timeToWait - timePassed);
 }
 
-// the db file will keep all the necessary info so that the API constraint is respected when the app is rebooted too.
+// the db file will keep all the necessary info so that the API constraint is 
+// respected when the app is rebooted too.
 function getDB(dbPath) {
   try {
     const db = require(dbPath);
@@ -125,7 +163,8 @@ function getDB(dbPath) {
   }
 }
 
-// returns a boolean telling whether value is a number or not, NaN is checked for too, I don't like it to break my programs
+// returns a boolean telling whether value is a number or not, NaN is checked 
+// for too, I don't like it to break my programs
 function isNumber(value) {
   return typeof value === "number" && value === value;
 }
@@ -144,7 +183,8 @@ function setDB(lr, lq) {
   return fs.writeFile(DB_LOCATION, JSON.stringify(body, null, 2), noAction);
 }
 
-// this function is used only to silence Node.js in seDB who threatens developers who don't handle async errors, we don't care about 
+// this function is used only to silence Node.js in seDB who threatens 
+// developers who don't handle async errors, we don't care about 
 function noAction() {}
 
 
